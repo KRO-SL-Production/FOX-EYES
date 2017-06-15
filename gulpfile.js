6c9a62ddf9ca28579e809543222d7eaf5329d35a
shinate <shine.wangrs@gmail.com>
2017-06-15 13:24:39 +0800

    123

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var gulpZip = require('gulp-zip');
 var gulpUnzip = require('gulp-unzip');
-var q = require('queue')();
+var q = require('d3-queue');
 var exec = require('child-process-promise').exec;
 
 gulp.task('zip', function () {
@@ -18,27 +18,27 @@ gulp.task('unzip', function () {
         .pipe(gulp.dest('src/design'));
 });
 
-gulp.task('save', function (cb) {
+gulp.task('save', ['zip'], function (cb) {
 
-    function due(command) {
-        return function (cb) {
-            exec(command)
-                .then(function (result) {
-                    console.log(result.stdout, result.stderr);
-                    cb();
-                })
-                .catch(function (err) {
-                    console.error('ERROR: ', err);
-                });
-        }
+    function due(command, next) {
+        console.log('Run command: ' + command);
+        exec(command)
+            .then(function (result) {
+                console.log(result.stdout, result.stderr);
+                next();
+            })
+            .catch(function (err) {
+                console.error('ERROR: ', err);
+            });
     }
 
-    q.push(
-        due('git add .'),
-        due('git commit -m \'save\''),
-        due('git push origin master:master')
-    );
-    q.start(function (err) {
-        cb();
-    });
+    q.queue()
+        .defer(due, 'git add .')
+        .defer(due, 'git commit -m \'save\'')
+        .defer(due, 'git push origin master:master')
+        .awaitAll(function (error) {
+            if (error) throw error;
+            console.log("Goodbye!");
+            cb();
+        });
 });
