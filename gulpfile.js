9d2d971e977c369f1b50ebf6852fbb7d6df26a8b
shinate <shine.wangrs@gmail.com>
2017-06-15 11:50:19 +0800

    save & update && git push origin master:master

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var gulpZip = require('gulp-zip');
 var gulpUnzip = require('gulp-unzip');
+var exec = require('child-process-promise').exec;
 
 gulp.task('zip', function () {
     return gulp.src('src/design/*')
@@ -14,4 +15,17 @@ gulp.task('unzip', function () {
     return gulp.src('src/design.zip')
         .pipe(gulpUnzip())
         .pipe(gulp.dest('src/design'));
+});
+
+gulp.task('save', function (cb) {
+    exec('git add . && git commit -m "save & update && git push origin master:master"')
+        .then(function (result) {
+            var stdout = result.stdout;
+            var stderr = result.stderr;
+            console.log('stdout: ', stdout);
+            console.log('stderr: ', stderr);
+        })
+        .catch(function (err) {
+            console.error('ERROR: ', err);
+        })
 });
